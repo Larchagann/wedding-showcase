e0032df960649107aec6fb8f75bdf85e18f7971e
Larchagann <antoine.houze@hotmail.com>
2024-02-18 16:32:57 +0100

    changement fonction isMobile

--- utils/utils.js
+++ utils/utils.js
@@ -4,27 +4,29 @@ export const checkValideAddressMail = (addressMail) => {
     return addressMail.match(addressMailRegex)
 }
 
-export const isMobile = () => {
-    return window.innerWidth <= 800 
-};
-
-// export const isMobile = {
-//     Android: function() {
-//         return navigator.userAgent.match(/Android/i);
-//     },
-//     BlackBerry: function() {
-//         return navigator.userAgent.match(/BlackBerry/i);
-//     },
-//     iOS: function() {
-//         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-//     },
-//     Opera: function() {
-//         return navigator.userAgent.match(/Opera Mini/i);
-//     },
-//     Windows: function() {
-//         return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-//     },
-//     any: function() {
-//         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-//     }
+// export const isMobile = () => {
+//     return window.innerWidth <= 800 
 // };
+
+export const isMobile = () => {
+    const isMobileCheck = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobileCheck.Android() || isMobileCheck.BlackBerry() || isMobileCheck.iOS() || isMobileCheck.Opera() || isMobileCheck.Windows());
+    }                      }
+    return isMobileCheck.any()
+};
