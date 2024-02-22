71b40271944c0db1f058b98983ddb1b2726435f7
Larchagann <antoine.houze@hotmail.com>
2024-02-22 09:00:06 +0100

    correction bannière en mobile

--- Components/banner/index.jsx
+++ Components/banner/index.jsx
@@ -4,9 +4,16 @@ import { isMobile } from "@/utils/utils";
 import styles from "./banner.module.scss";
 import banner from "../../images/banner.png";
 import mobileBanner from "../../images/mobileBanner.png";
+import { useEffect, useState } from "react";
 
 export default function Banner() {
-  return isMobile() ? (
+  const [useMobile, setUseMobile] = useState(false)
+
+  useEffect(() => {
+    setUseMobile(isMobile())
+  }, [])
+
+  return useMobile ? (
     <img className={styles.banner} src={mobileBanner.src} alt="mobileBanner" />
   ) : (
     <img className={styles.banner} src={banner.src} alt="banner" />
