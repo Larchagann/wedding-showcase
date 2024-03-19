5fea6d77592998ab63fe5586a4363efdede658e7
Larchagann <antoine.houze@hotmail.com>
2024-03-19 15:39:17 +0100

    ajout msg att pas divulg lieu rec

--- Components/layout/footer/index.jsx
+++ Components/layout/footer/index.jsx
@@ -6,9 +6,13 @@ import { FaRegCopyright } from "react-icons/fa6";
 import { isMobile } from "@/utils/utils";
 
 export default function Footer() {
-  return <div className={styles.footer}>
-    <FaRegCopyright size={12} className={styles.icon}/>
-    {`- Wedding Showcase by Coding-Cow`}
-    {isMobile() ? <div className={styles.isMobileFooter} /> : <></>}
-  </div>;
+  return (
+    <>
+      <div className={styles.footer}>
+        <FaRegCopyright size={12} className={styles.icon} />
+        {`- Wedding Showcase by Coding-Cow`}
+      </div>
+      {isMobile() ? <div className={styles.isMobileFooter}></div> : <></>}
+    </>
+  );
 }
--- Components/placesCards/index.jsx
+++ Components/placesCards/index.jsx
@@ -1,9 +1,10 @@
 "use client";
 
 import React from "react";
-import { Grid } from "@mui/material";
+import { Card, CardContent, Grid } from "@mui/material";
 import { useUserContext } from "@/context/context";
 import PlacesCard from "../placesCard";
+import styles from "../../styles/card.module.scss"
 
 export default function PlacesCards() {
   const context = useUserContext();
@@ -12,6 +13,17 @@ export default function PlacesCards() {
     <Grid container spacing={2}>
       {context.user.isCityHallInvited ? (
         <>
+          <Grid item xs={12} md={12}>
+            <Card className={styles.card}>
+              <CardContent className={styles.cardContent}>
+                <div className={styles.alertRed}>
+                  Merci de garder le lieu de la réception, de la mairie et
+                  l&apos;heure de la mairie confidentiel pour des questions de
+                  logistique !
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
           <Grid item xs={12} md={6}>
             <PlacesCard
               headerText={"Mairie"}
@@ -29,13 +41,25 @@ export default function PlacesCards() {
           </Grid>
         </>
       ) : (
-        <Grid item xs={12} md={12}>
-          <PlacesCard
-            headerText="Réception"
-            placeName="SALLE DES FÊTES DE SENNECÉ"
-            address="RUE VRÉMONTOISE 71000 MÂCON"
-          />
-        </Grid>
+        <>
+          <Grid item xs={12} md={12}>
+            <Card className={styles.card}>
+              <CardContent className={styles.cardContent}>
+                <div className={styles.alertRed}>
+                  Merci de garder le lieu de la réception confidentiel pour des
+                  questions de logistique !
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <PlacesCard
+              headerText="Réception"
+              placeName="SALLE DES FÊTES DE SENNECÉ"
+              address="RUE VRÉMONTOISE 71000 MÂCON"
+            />
+          </Grid>
+        </>
       )}
     </Grid>
   );
