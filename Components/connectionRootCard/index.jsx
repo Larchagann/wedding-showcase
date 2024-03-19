9e6a41c9d66ff94e3f97f7fe48c14445ca29e243
Larchagann <antoine.houze@hotmail.com>
2024-03-19 15:17:02 +0100

    ajout verification token minute

--- Components/connectionRootCard/index.jsx
+++ Components/connectionRootCard/index.jsx
@@ -7,14 +7,19 @@ import { verify } from "jsonwebtoken";
 export default function ConnectionRootCard({ headerText, children }) {
   const context = useUserContext();
 
+  const checkValidity = () => {
+    try {
+      verify(context.token, process.env.NEXT_PUBLIC_API_JWT_KEY);
+      setTimeout(() => {checkValidity()}, 60000)
+    } catch (error) {
+      console.log("SESSION EXPIRED");
+      context.logout();
+    }
+  }
+
   useEffect(() => {
     if (context.token != null) {
-      try {
-        verify(context.token, process.env.NEXT_PUBLIC_API_JWT_KEY);
-      } catch (error) {
-        console.log("SESSION EXPIRED");
-        context.logout();
-      }
+      checkValidity()
     }
   }, [context.user, context.token]);
 
